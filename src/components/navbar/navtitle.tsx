aa669fda1fb3b8f4819c4ac694bc2f35d3b9f20b
brockgingell <brockg68@icloud.com>
2023-10-04 01:25:43 -0400

    removed icon.tsx changed name in title

--- src/components/navbar/navtitle.tsx
+++ src/components/navbar/navtitle.tsx
@@ -1,14 +1,14 @@
 "use client"
 import Image from 'next/image'
 import Link from 'next/link'
-import { CompanyIcon } from '@/components/ui/company-icon'
+import CompanyIcon from 'public/icon.svg'
 
 export default function NavTitle() {
   return (
     <section>
       <Link href="/" className='flex flex-row items-center gap-[3px]'>
         <CompanyIcon className='h-[50px] md:h-[72px] xl:h-[80px]'/>
-        <h1 className='font-bold leading-[17px] tracking-tighter md:text-xl md:leading-[24px] xl:text-2xl xl:leading-[26px]'>Upstate<br/>Digital<br/>Solutions</h1>
+        <h1 className='font-bold leading-[17px] tracking-tighter md:text-xl md:leading-[24px] xl:text-2xl xl:leading-[26px]'>Upstate<br/>Digital<br/>Design</h1>
       </Link>
     </section>
   )
