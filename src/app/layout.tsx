79a31c05446815c616327632fda90b6bcd3ece0d
brockgingell <brockg68@icloud.com>
2023-10-04 00:51:50 -0400

    moved meta from layout to manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,9 @@
 import '@/styles/globals.css'
-import type { Metadata } from 'next'
-import { siteConfig } from '@/config/site'
-import { fontSans } from '@/lib/fonts'
 import { cn } from '@/lib/utils'
+import { fontSans } from '@/lib/fonts'
 import { TailwindIndicator } from '@/components/ui/tailwind-indicator'
 import { ThemeProvider } from '@/components/ui/theme-provider'
 import { Analytics } from '@vercel/analytics/react'
-
-
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-}
 
 interface RootLayoutProps {
   children: React.ReactNode
