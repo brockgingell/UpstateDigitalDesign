63ef2b53aa76b5a3aee0ea788ca4ca5c68e19dde
brockgingell <brockg68@icloud.com>
2023-10-04 02:29:34 -0400

    weird

--- src/components/navbar/navtitle.tsx
+++ src/components/navbar/navtitle.tsx
@@ -1,12 +1,13 @@
+"use client"
 import Image from 'next/image'
 import Link from 'next/link'
-import NavIcon from 'public/icon.svg'
+import { CompanyIcon } from '@/components/ui/company-icon'
 
 export default function NavTitle() {
   return (
     <section>
       <Link href="/" className='flex flex-row items-center gap-[3px]'>
-        <NavIcon className='h-[50px] md:h-[60px] xl:h-[69px]' /> 
+        <CompanyIcon className='h-[50px] md:h-[60px] xl:h-[69px]'/>
         <h1 className='font-bold leading-[17px] tracking-tighter md:text-lg md:leading-[20px] xl:text-2xl xl:leading-[26px]'>Upstate<br/>Digital<br/>Design</h1>
       </Link>
     </section>
