eda587b3353530970aee95bea56d01614f85cd90
brockgingell <brockg68@icloud.com>
2023-10-04 00:38:21 -0400

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,17 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <head/>
+        <head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/site.webmanifest"/>
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#454545"/>
+          <meta name="apple-mobile-web-app-title" content="UpstateDigitalDesign"/>
+          <meta name="application-name" content="UpstateDigitalDesign"/>
+          <meta name="msapplication-TileColor" content="#da532c"/>
+          <meta name="theme-color" content="#ffffff"/>
+        </head>
         <body className={cn("min-h-screen bg-background antialiased", fontSans.className)}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="min-h-screen flex-1 flex-col">
